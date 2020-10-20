a8fd3044f4b9532ad83788868302f9ff8ed8f49a
Khalitov Adel <adelkhalitov1@gmail.com>
2020-10-21 00:33:24 +0300

    added login from config

--- src/instagram/page-statistic/page-statistic.module.ts
+++ src/instagram/page-statistic/page-statistic.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { InstagramPageModule } from '../instagram-page/instagram-page.module';
 import { InstagramPageService } from '../instagram-page/instagram-page.service';
+import { auth } from '../../config';
 
 @Module({
   imports: [InstagramPageModule],
@@ -8,7 +9,7 @@ import { InstagramPageService } from '../instagram-page/instagram-page.service';
     {
       provide: 'PAGE',
       useFactory: async (service: InstagramPageService) => {
-        return await service.login('khalitov_adel', 'adelADEL131!');
+        return await service.login(auth.login, auth.password);
       },
       inject: [InstagramPageService]
     },
